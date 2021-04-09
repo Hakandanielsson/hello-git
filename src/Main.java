46ec7346f511108d26d69a2eb0c8f503950cbc05
Hakan Danielsson <hakan.danielsson@hm.com>
2021-04-09 14:22:57 +0200

    decrement added

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,8 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello Git!");
     }
+
+    public static int decrement(int num) {
+        return num -1;
+    }
 }
